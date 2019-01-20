6bced19d4efdca2dbd55c084c054a292514d8ccd
contrerasjf <contrerasjf.0@hotmail.com>
2019-01-19 21:47:37 -0600

    Feature(Users):Implement the friends list in the home
    * The frinds list was implemented in the component home with the directives NgFor and NgIf.
    
    Resolve: c17, c18

--- src/app/interfaces/user.ts
+++ src/app/interfaces/user.ts
@@ -1,8 +1,9 @@
 export interface User {
-    nick: string,
-    subnick?: string,
-    age?: number,
-    email: string,
-    friend: boolean,
-    uid: any
+    nick: string;
+    subnick?: string;
+    age?: number;
+    email: string;
+    friend: boolean;
+    uid: any;
   }
+  
